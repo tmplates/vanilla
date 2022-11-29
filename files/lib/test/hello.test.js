0bba30cfa72d14c3d9ee7d7ff18a70df3f5dc7c2
2046 <g@ggg.gd>
2022-11-29 12:12:33 +0800

    feat: update preset template

--- files/lib/test/hello.test.js
+++
@@ -1,7 +0,0 @@
-const hello = require('../src')
-
-describe('Hello cases', () => {
-  test('must be return the string value', () => {
-    expect(hello('World!')).toBe('Hello World!')
-  })
-})
--- index.js
+++ index.js
@@ -9,40 +9,24 @@ module.exports = async function(prompts) {
     message: 'Create a library project?',
   })
 
-  const ts = await prompts({
-    name: 'value',
-    initial: true,
-    type: 'confirm',
-    message: 'Use the TypeScript language?',
-  })
-
   if (lib.value) {
     return {
+      ts: true,
       lib: true,
       test: true,
-      ts: ts.value,
       dirs: ['src'],
       framework: 'vanilla',
-      files: ts.value ? [
+      files: [
         ['src/index.ts', fs.readFileSync(resolve('files/lib/src/index.ts'))],
         ['test/hello.test.ts', fs.readFileSync(resolve('files/lib/test/hello.test.ts'))]
-      ] : [
-        ['src/index.js', fs.readFileSync(resolve('files/lib/src/index.js'))],
-        ['test/hello.test.js', fs.readFileSync(resolve('files/lib/test/hello.test.js'))]
       ],
       lint: ['stylelint', 'eslint', 'commitlint']
     }
   } else {
     return {
-      ts: ts.value,
       dirs: ['src'],
       framework: 'vanilla',
-      files: ts.value ? [
-        ['src/index.ejs', fs.readFileSync(resolve('files/business/index.ejs'))],
-        ['src/assets/js/index.ts', fs.readFileSync(resolve('files/business/assets/js/index.ts'))],
-        ['src/assets/css/base.css', fs.readFileSync(resolve('files/business/assets/css/base.css'))],
-        ['src/assets/css/index.scss', fs.readFileSync(resolve('files/business/assets/css/index.scss'))]
-      ] : [
+      files: [
         ['src/index.ejs', fs.readFileSync(resolve('files/business/index.ejs'))],
         ['src/assets/js/index.js', fs.readFileSync(resolve('files/business/assets/js/index.js'))],
         ['src/assets/css/base.css', fs.readFileSync(resolve('files/business/assets/css/base.css'))],
